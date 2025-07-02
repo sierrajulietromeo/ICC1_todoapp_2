6e3c77d688011b24bd6684abf726933d2f55a8cd
Steve Rich <steven.john.rich@gmail.com>
2025-07-02 08:17:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ def delete_task(task_id):
     return redirect(url_for('tasks'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8080)
